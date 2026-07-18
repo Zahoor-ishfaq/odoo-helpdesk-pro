3ed2ac51ec1d02153f4c7877d1bd549d68ea7724
Zahoor Ishfaq <zahoor.ishfaaq@gmail.com>
2026-07-18 10:37:28 +0300

    [ADD] helpdesk_pro module skeleton

---
+++ helpdesk_pro/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Helpdesk Pro - SLA, Ratings & Email Support",
+    "summary": "Enterprise-grade helpdesk: SLA engine, CSAT ratings, "
+    "email-to-ticket, customer portal",
+    "version": "17.0.1.0.0",
+    "category": "Services/Helpdesk",
+    "author": "Zahoor Ishfaq",
+    "website": "https://github.com/Zahoor-ishfaq/odoo-helpdesk-pro",
+    "license": "LGPL-3",
+    "depends": ["mail", "portal", "resource"],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "application": True,
+}
